0c27effa29eec509aa1c4d71f238a421d7effe83
afrnmohsin <76119725+afrnmohsin@users.noreply.github.com>
2021-01-28 23:04:41 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var fr,mr;
+
+
+function setup() {
+  var canvas = createCanvas(400,400);
+  fr=createSpite
+ 
+}
+
+function draw() {
+  background(0);  
+  rectMode(CENTER);
+  rect(200,200,50,50);
+  drawSprites();
+}
